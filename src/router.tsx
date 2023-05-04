50295d048329f57918f7294478b73634b5246d1e
roscoevanderboom <roscoevanderboom@protonmail.com>
2023-05-04 17:16:35 +0800

    change to createMemoryRouter

--- src/router.tsx
+++ src/router.tsx
@@ -1,35 +1,39 @@
-import { Navigate, createBrowserRouter } from "react-router-dom";
+import { Navigate, createMemoryRouter } from "react-router-dom";
 import App from "./pages/App";
 import Login from "./pages/login";
 import Home from "./pages/home";
 import Profile from "./pages/home/profile";
 import Settings from "./pages/home/settings";
 
-export default createBrowserRouter([
+export default createMemoryRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          path: "login",
+          element: <Login />,
+        },
+        {
+          path: "home",
+          element: <Home />,
+          children: [
+            { index: true, element: <Navigate to="home/profile" replace /> },
+            {
+              path: "profile",
+              element: <Profile />,
+            },
+            {
+              path: "settings",
+              element: <Settings />,
+            },
+          ],
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <App />,
-    children: [
-      { index: true, element: <Navigate to="/login" replace /> },
-      {
-        path: "login",
-        element: <Login />,
-      },
-      {
-        path: "home",
-        element: <Home />,
-        children: [
-          { index: true, element: <Navigate to="home/profile" replace /> },
-          {
-            path: "profile",
-            element: <Profile />,
-          },
-          {
-            path: "settings",
-            element: <Settings />,
-          },
-        ],
-      },
-    ],
-  },
-]);
+    initialEntries: ["/login"],
+  }
+);
